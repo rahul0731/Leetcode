72ba2f7770e070b3de579a2321a393f31b861b9e
rahul0731 <38300938+rahul0731@users.noreply.github.com>
2020-04-12 12:52:47 +0530

    Update Day11.py

--- 30DaysChallenge/Python3/Day11.py
+++ 30DaysChallenge/Python3/Day11.py
@@ -1,56 +1,12 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class Solution(object):
+    def diameterOfBinaryTree(self, root):
+        self.ans = 1
+        def depth(node):
+            if not node: return 0
+            L = depth(node.left)
+            R = depth(node.right)
+            self.ans = max(self.ans, L+R+1)
+            return max(L, R) + 1
 
-class Solution:
-    def diameterOfBinaryTree(self, root: TreeNode) -> int:
-        farthestNode = None
-        farthestDistance = 0
-        
-        def dfsFromRoot(node ,parent ,distance):
-             
-            nonlocal farthestDistance
-            nonlocal farthestNode
-            if node is None:
-                return 
-            if distance > farthestDistance:
-                farthestDistance = distance
-                farthestNode = node
-                
-           # print(node ,distance , farthestDistance , farthestNode)    
-            node.parent = parent
-        
-            dfsFromRoot(node.left, node, distance + 1)
-            dfsFromRoot(node.right, node, distance + 1)
-        
-        dfsFromRoot(root , None ,0)
-        #print(fathestDistance, farthestNode)
-    
-        farthestDistanceSecond= 0
-    
-        def dfs(node ,distance):
-            nonlocal farthestDistanceSecond
-      
-            if node is None:
-                 return 
-            if hasattr(node, 'used'):
-                 return
-            if distance > farthestDistanceSecond:
-                 farthestDistanceSecond= distance
-            
-            node.used = True
-        
-            dfs(node.left ,distance + 1)
-            dfs(node.right ,distance + 1)
-            dfs(node.parent ,distance + 1)
-        
-        
-        dfs(farthestNode, 0)
-        return farthestDistanceSecond
-    
-    
-    
-    
+        depth(root)
+        return self.ans - 1
