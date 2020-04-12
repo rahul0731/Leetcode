c5d9631a6fbe95b2d09bafa223437b9f2dbd49ee
rahul0731 <38300938+rahul0731@users.noreply.github.com>
2020-04-12 12:55:03 +0530

    Update Day8.py

--- 30DaysChallenge/Python3/Day8.py
+++ 30DaysChallenge/Python3/Day8.py
@@ -1,35 +1,7 @@
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
-class Solution:
-    def middleNode(self, head: ListNode) -> ListNode:
-        first= ListNode()
-        first.next = head
-        
-        faster = first
-        slower = first
-        
-        while faster is not None:
-            if faster.next is not None:
-                faster = faster.next
-            faster = faster.next
-            slower = slower.next
-            
-        return slower
-    
-        N = 0
-        pointer = head
-        while pointer is not None:
-            pointer = pointer.next
-            N += 1
-        
-        pointer = head
-        N //= 2
-        while N > 0:
-            pointer = pointer.next
-            N -= 1
-        return pointer
-        
+class Solution(object):
+    def middleNode(self, head):
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
