5dba6ed829dd96bf6ddf0509e5bb498cd413abca
rahul0731 <38300938+rahul0731@users.noreply.github.com>
2020-04-12 12:56:12 +0530

    Update Day9.py

--- 30DaysChallenge/Python3/Day9.py
+++ 30DaysChallenge/Python3/Day9.py
@@ -1,13 +1,12 @@
-class Solution:
-    def backspaceCompare(self, S: str, T: str) -> bool:
-        def convert(word):
-            stack = []
-            for c in word:
-                if c ==  "#":
-                    if len(stack)>0:
-                        stack.pop()
-                else:
-                    stack.append(c)
-            return "".join(stack)
-        
-        return convert(S) == convert(T)
+  
+    class Solution(object):
+    def backspaceCompare(self, S, T):
+        def build(S):
+            ans = []
+            for c in S:
+                if c != '#':
+                    ans.append(c)
+                elif ans:
+                    ans.pop()
+            return "".join(ans)
+        return build(S) == build(T)
