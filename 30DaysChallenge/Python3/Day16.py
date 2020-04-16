e2577f0bd77f164efdd19ba0158a5d6080904815
rahul0731 <38300938+rahul0731@users.noreply.github.com>
2020-04-16 14:58:53 +0530

    Update Day16.py

--- 30DaysChallenge/Python3/Day16.py
+++ 30DaysChallenge/Python3/Day16.py
@@ -1,23 +1,10 @@
-from functools import lru_cache
-class Solution:
-    def checkValidString(self, s: str) -> bool:
-        N = len(s)
-        
-        @lru_cache(None)
-        def isValid(index, depth):
-            if index == N:
-                if depth == 0:
-                    return True
-                else:
-                    return False
-            if depth < 0:
-                return False
-            
-            if s[index] == '(':
-                return isValid(index + 1, depth + 1)
-            elif s[index] == ')':
-                return isValid(index + 1 ,depth - 1)
-            else:
-                return isValid(index +1 ,depth +1) or isValid(index + 1 ,depth) or isValid(index + 1 ,depth - 1)
-           
-        return isValid(0,0)
+class Solution(object):
+    def checkValidString(self, s):
+        lo = hi = 0
+        for c in s:
+            lo += 1 if c == '(' else -1
+            hi += 1 if c != ')' else -1
+            if hi < 0: break
+            lo = max(lo, 0)
+
+        return lo == 0
